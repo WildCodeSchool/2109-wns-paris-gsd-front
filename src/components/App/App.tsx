d934b8e0a5bbfd5ce94091ee59f28b9b290de0f5
Camillodo <camille.doizelet@gmail.com>
2021-11-26 10:29:23 +0100

    changement image logo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Login from '../Login/Login'
 import Dashboard from '../Dashboard/Dasboard'
 
