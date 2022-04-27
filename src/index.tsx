93b05c4f6cac8530401b9d6399e4f9b3a4f86095
Eurico Da Custodia <rico.alves91@gmail.com>
2022-04-27 11:24:30 +0200

    ..... it s me mario the dumb plumber

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,8 @@ import { AuthProvider } from './hooks/useAuth';
 
 
 const httpLink = createHttpLink({
-  uri: `${process.env.GRAPHL_SERVER_URL}/graphql`,
+  // uri: `${process.env.GRAPHL_SERVER_URL}/graphql`,
+  uri: `https://staging.paris3-0921.wns.wilders.dev/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
