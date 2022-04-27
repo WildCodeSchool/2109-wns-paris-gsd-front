32edcecac2895dc8aa9d316397e11406451104c2
vde-guil <valentin.de.guillebon@gmail.com>
2022-04-27 17:04:12 +0200

    t'as pas dit bonjour

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -43,7 +43,7 @@ const Login: React.FC = () => {
 
   return (
     <>
-    <div>Ciel, mes bijoux. - La Castafiore</div>
+    <div>coke en stock dans mon nez - vald</div>
       <div className="asterisk">
         <div className="asterisk_single">*</div>
         <div className="asterisk_single">*</div>
