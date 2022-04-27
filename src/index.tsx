764c34cf1ccfd88be1507a0f04f26c06f9f962d8
vde-guil <valentin.de.guillebon@gmail.com>
2022-04-27 14:56:24 +0200

    test(deploy): front uri /grapql

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import { AuthProvider } from './hooks/useAuth';
 const httpLink = createHttpLink({
   // uri: `${process.env.GRAPHL_SERVER_URL}/graphql`,
   // uri: `https://staging.paris3-0921.wns.wilders.dev/graphql`,
-  uri: `http://localhost:8000/graphql`,
+  uri: `/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
